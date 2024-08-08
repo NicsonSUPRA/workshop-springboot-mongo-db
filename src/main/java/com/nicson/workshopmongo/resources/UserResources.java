e8607f55ced0a2099f287caf0de75ce2491d8485
NicsonSUPRA <nicsoncc1234@gmail.com>
2024-08-08 14:44:30 -0300

    inserção de usuário com post

--- src/main/java/com/nicson/workshopmongo/resources/UserResources.java
+++ src/main/java/com/nicson/workshopmongo/resources/UserResources.java
@@ -1,13 +1,17 @@
 package com.nicson.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nicson.workshopmongo.domains.User;
 import com.nicson.workshopmongo.dto.UserDTO;
@@ -25,6 +29,20 @@ public class UserResources {
         List<User> list = services.findAll();
         List<UserDTO> listDTO = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = services.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+        User obj = services.fromDTO(userDTO);
+        obj = services.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/nicson/workshopmongo/services/UserServices.java
+++ src/main/java/com/nicson/workshopmongo/services/UserServices.java
@@ -1,12 +1,16 @@
 package com.nicson.workshopmongo.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nicson.workshopmongo.domains.User;
+import com.nicson.workshopmongo.dto.UserDTO;
 import com.nicson.workshopmongo.repositories.UserRepository;
+import com.nicson.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserServices {
@@ -16,5 +20,22 @@ public class UserServices {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        try {
+            Optional<User> user = repository.findById(id);
+            return user.get();
+        } catch (NoSuchElementException e) {
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+    }
+
+    public User insert(User user) {
+        return repository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
