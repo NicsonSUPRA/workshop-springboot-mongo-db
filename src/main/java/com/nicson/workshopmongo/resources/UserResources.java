48bf5a90e2122172a8777caa4dbbb5d312fc873f
NicsonSUPRA <nicsoncc1234@gmail.com>
2024-08-08 12:50:33 -0300

    implementada classe UserDTO

--- src/main/java/com/nicson/workshopmongo/resources/UserResources.java
+++ src/main/java/com/nicson/workshopmongo/resources/UserResources.java
@@ -1,6 +1,7 @@
 package com.nicson.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nicson.workshopmongo.domains.User;
+import com.nicson.workshopmongo.dto.UserDTO;
 import com.nicson.workshopmongo.services.UserServices;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResources {
     private UserServices services;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = services.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
 }
